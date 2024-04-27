07d660a06c558af2a62c725f634465ea9d301fad
bashar <basharhasan74@gmail.com>
2024-04-27 23:08:03 +0300

    feature(api): move logic to service layer

--- src/api/knn.py
+++ src/api/knn.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter
-import numpy as np
-import pandas as pd
 from pydantic import NonNegativeInt
 from schemas.mushroom import Mushroom
 from services.knn import KNNService
@@ -16,27 +14,8 @@ async def predict(
     mushroom: Mushroom,
     k: NonNegativeInt = 3,
 ):
-    df: pd.DataFrame = pd.read_csv(filepath_or_buffer="knn.csv")
-    X_train = df.iloc[:, :-1].values
-    y_train = df.iloc[:, -1].values
-    X_test = np.array(
-        object=[
-            [
-                mushroom.cap_diameter,
-                mushroom.cap_shape,
-                mushroom.gill_attachment,
-                mushroom.gill_color,
-                mushroom.stem_height,
-                mushroom.stem_width,
-                mushroom.stem_color,
-                mushroom.season,
-            ]
-        ]
-    )
-    prediction = KNNService().knn_predict(
-        X_train=X_train,
-        y_train=y_train,
-        X_test=X_test,
+    prediction = KNNService().knn(
+        mushroom_data=mushroom,
         k=k,
     )
     return {"prediction": "Edible" if prediction[0] == 0 else "Not Edible"}
