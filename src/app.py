4b0b1f3918381e585db11aaf00cfa34ff703fe58
bashar <basharhasan74@gmail.com>
2024-04-28 17:01:29 +0300

    feature: add brotli and orjson

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from fastapi.responses import ORJSONResponse
+from brotli_asgi import BrotliMiddleware
 from api.apriori import apriori_router
 from api.knn import knn_router
 
@@ -8,8 +9,14 @@ def app_factory() -> FastAPI:
     fastapi_app = FastAPI(
         title="Data Mining Project",
         version="0.0.1",
+        default_response_class=ORJSONResponse,
     )
 
+    fastapi_app.add_middleware(
+        middleware_class=BrotliMiddleware,
+        quality=6,
+        minimum_size=1000,
+    )
     fastapi_app.include_router(router=apriori_router)
     fastapi_app.include_router(router=knn_router)
 
