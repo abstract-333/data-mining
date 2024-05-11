14dc71fd9e022000d43d6465762fe4f0dba16efb
bashar <basharhasan74@gmail.com>
2024-05-11 15:16:10 +0300

    feature(api): make KNNRouter class that includes knn routes

--- src/api/knn.py
+++ src/api/knn.py
@@ -3,19 +3,27 @@ from pydantic import NonNegativeInt
 from schemas.mushroom import Mushroom
 from services.knn import KNNService
 
-knn_router = APIRouter(
-    prefix="/knn",
-    tags=["K-NN"],
-)
 
+class KNNRouter:
+    def __init__(self) -> None:
+        self.knn_service = KNNService()
+        self.router = APIRouter(
+            prefix="/knn",
+            tags=["K-NN"],
+        )
+        self.router.add_api_route(
+            path="",
+            endpoint=self.classify_mushroom,
+            methods=["Post"],
+        )
 
-@knn_router.post("")
-async def classify_mushroom(
-    mushroom: Mushroom,
-    k: NonNegativeInt = 3,
-):
-    classification = KNNService().knn(
-        mushroom_data=mushroom,
-        k=k,
-    )
-    return {"prediction": "Edible" if classification[0] == 0 else "Not Edible"}
+    async def classify_mushroom(
+        self,
+        mushroom: Mushroom,
+        k: NonNegativeInt = 3,
+    ):
+        classification = self.knn_service.knn(
+            mushroom_data=mushroom,
+            k=k,
+        )
+        return {"prediction": "Edible" if classification[0] == 0 else "Not Edible"}
